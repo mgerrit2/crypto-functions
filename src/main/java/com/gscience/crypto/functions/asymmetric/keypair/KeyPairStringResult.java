48ca06613cd417239dff163468113649f35ee7ba
Marc Gerrits <gerrits.marc@hotmail.com>
2026-06-24 23:26:01 +0200

    maitenance: commit

--- src/main/java/com/gscience/crypto/functions/asymmetric/keypair/KeyPairStringResult.java
+++ src/main/java/com/gscience/crypto/functions/asymmetric/keypair/KeyPairStringResult.java
@@ -1,12 +1,9 @@
-package com.gscience.crypto.Crypto_functions.AsymmetricKeyPair;
+package com.gscience.crypto.functions.asymmetric.keypair;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.security.PrivateKey;
-import java.security.PublicKey;
 
 @AllArgsConstructor
 @NoArgsConstructor
